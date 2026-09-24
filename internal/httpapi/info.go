956dc65279deeb8f981417eec3e3d51f1d532767
Ali <ali.bitcoin.cyber@gmail.com>
2026-09-24 09:44:07 +0430

    release: align HTTP runtime version with 0.4.2

--- internal/httpapi/info.go
+++ internal/httpapi/info.go
@@ -5,7 +5,7 @@ import (
 	"runtime"
 )
 
-const gatewayVersion = "0.4.1"
+const gatewayVersion = "0.4.2"
 
 func (s *Server) hello(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
